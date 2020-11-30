c0716c67e2898dedf8ca88e7c3495fecbb9512ff
Lucas Ansei <lucas.ansei@appcivico.com>
2020-11-30 16:00:55 -0300

    Updating ci script

--- script/ci/jenkins-run-persistent-env.sh
+++ script/ci/jenkins-run-persistent-env.sh
@@ -3,7 +3,7 @@ export INSIDE_WORKSPACE=/var/jenkins_home/dev-persistent/$JOB_NAME
 
 mkdir -p $INSIDE_WORKSPACE/src
 mkdir -p $INSIDE_WORKSPACE/data
-chown 1000:1000 $INSIDE_WORKSPACE/data -R
+# chown 1000:1000 $INSIDE_WORKSPACE/data -R
 
 rsync -av $WORKSPACE/ $INSIDE_WORKSPACE/src;
 
