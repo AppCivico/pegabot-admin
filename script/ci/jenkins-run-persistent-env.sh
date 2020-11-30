c5b6c8c5ddbbc4d4ba9c93fcb9bb8ed01c444088
Lucas Ansei <lucas.ansei@appcivico.com>
2020-11-30 15:28:03 -0300

    Adding ci script

---
+++ script/ci/jenkins-run-persistent-env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+export INSIDE_WORKSPACE=/var/jenkins_home/dev-persistent/$JOB_NAME
+
+mkdir -p $INSIDE_WORKSPACE/src
+mkdir -p $INSIDE_WORKSPACE/data
+chown 1000:1000 $INSIDE_WORKSPACE/data -R
+
+rsync -av $WORKSPACE/ $INSIDE_WORKSPACE/src;
+
+cd $INSIDE_WORKSPACE/src;
+
+docker-compose up -d --build
