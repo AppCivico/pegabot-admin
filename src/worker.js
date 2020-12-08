22b1e31c17932b5eee3a03828e6693d16b629ed2
Junior M <juniorfvox@gmail.com>
2020-12-08 08:50:47 -0300

    feat: get queue pending items.

--- src/worker.js
+++ src/worker.js
@@ -1,31 +1,56 @@
 import Queue from 'bull';
 import Debug from 'debug';
-import directus from './directus';
+import Directus from 'src/directus';
+import { userRequestsCollection } from 'src/collections';
 
 const debug = Debug('pegabot-admin:app');
 
-// TODO Use the env
-const queue = new Queue('default', { redis: { port: 6379, host: '127.0.0.1' } });
+(async () => {
+  // TODO Use the env
+  const queue = new Queue('default', { redis: { port: 6379, host: '127.0.0.1' } });
 
-const pendingFiles = async () => {
-  // Get directus client
-  const client = await directus();
-  debug('%o', client);
-};
+  const directus = await Directus();
 
-const listenQueue = async (job, done) => {
-  debug('Check if there are files pending...');
-  const files = await pendingFiles();
-  debug(files);
+  const pendingFiles = async () => {
+    // Get directus client
+    const items = await directus.getItems(userRequestsCollection, { filter: { status: { eq: 'waiting' } } });
+    if (!items || !items.data || items.data.length === 0) {
+      debug('There is no files with status "waiting"');
+      return [];
+    }
 
-  done();
-};
+    const { data: fileList } = await directus.getFiles({ limit: -1 });
 
+    const files = [];
+    items.data.forEach((item) => {
+      const file = fileList.find((upload) => upload.id === item.input_file);
+      if (file) {
+        file.itemId = item.id;
+        files.push(file);
+      }
+    });
+    return files;
+  };
+
+  const listenQueue = async (job, done) => {
+    debug('Check if there are files pending...');
+    const files = await pendingFiles();
+    debug(files);
+
+    done();
+  };
+
+  queue.add();
+
+  queue.process(listenQueue);
+  debug('Listening queue...');
+})().catch((err) => {
+  // TODO Handle this exception properly
+  console.log(err);
+});
 // queue.add({}, { repeat: { cron: '* * * * *' } });
-queue.add();
 
-queue.process(listenQueue);
-debug('Listening queue...');
+
 
 // import { queues } from './queues';
 // import { processorInitialisers } from './processors';
